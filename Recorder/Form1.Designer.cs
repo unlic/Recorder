dbaf0d5ae453e03bfb8bc30321c666423b4f9cad
unlic <82399707+unlic@users.noreply.github.com>
2021-07-03 16:00:51 +0300

    Добавьте файлы проекта.

---
+++ Recorder/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Recorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mStrip = new System.Windows.Forms.MenuStrip();
+            this.b_SaveTo = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.videoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cBoxVideo = new System.Windows.Forms.ToolStripComboBox();
+            this.audioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cBoxAudio = new System.Windows.Forms.ToolStripComboBox();
+            this.b_Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.pBoxCam = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.b_Record = new System.Windows.Forms.Button();
+            this.b_Start = new System.Windows.Forms.Button();
+            this.mStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCam)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mStrip
+            // 
+            this.mStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.b_SaveTo,
+            this.toolStripMenuItem1,
+            this.b_Exit});
+            this.mStrip.Location = new System.Drawing.Point(0, 0);
+            this.mStrip.Name = "mStrip";
+            this.mStrip.Size = new System.Drawing.Size(484, 24);
+            this.mStrip.TabIndex = 0;
+            this.mStrip.Text = "menuStrip1";
+            // 
+            // b_SaveTo
+            // 
+            this.b_SaveTo.Name = "b_SaveTo";
+            this.b_SaveTo.Size = new System.Drawing.Size(58, 20);
+            this.b_SaveTo.Text = "Save To";
+            this.b_SaveTo.Click += new System.EventHandler(this.b_SaveTo_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.videoToolStripMenuItem,
+            this.audioToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(56, 20);
+            this.toolStripMenuItem1.Text = "Setting";
+            // 
+            // videoToolStripMenuItem
+            // 
+            this.videoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cBoxVideo});
+            this.videoToolStripMenuItem.Name = "videoToolStripMenuItem";
+            this.videoToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
+            this.videoToolStripMenuItem.Text = "Video";
+            // 
+            // cBoxVideo
+            // 
+            this.cBoxVideo.Name = "cBoxVideo";
+            this.cBoxVideo.Size = new System.Drawing.Size(121, 23);
+            // 
+            // audioToolStripMenuItem
+            // 
+            this.audioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cBoxAudio});
+            this.audioToolStripMenuItem.Name = "audioToolStripMenuItem";
+            this.audioToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
+            this.audioToolStripMenuItem.Text = "Audio";
+            // 
+            // cBoxAudio
+            // 
+            this.cBoxAudio.Name = "cBoxAudio";
+            this.cBoxAudio.Size = new System.Drawing.Size(121, 23);
+            // 
+            // b_Exit
+            // 
+            this.b_Exit.Name = "b_Exit";
+            this.b_Exit.Size = new System.Drawing.Size(38, 20);
+            this.b_Exit.Text = "Exit";
+            this.b_Exit.Click += new System.EventHandler(this.b_Exit_Click);
+            // 
+            // pBoxCam
+            // 
+            this.pBoxCam.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pBoxCam.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pBoxCam.Location = new System.Drawing.Point(0, 28);
+            this.pBoxCam.Name = "pBoxCam";
+            this.pBoxCam.Size = new System.Drawing.Size(484, 362);
+            this.pBoxCam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pBoxCam.TabIndex = 1;
+            this.pBoxCam.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.b_Record);
+            this.panel1.Controls.Add(this.b_Start);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 387);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(484, 63);
+            this.panel1.TabIndex = 2;
+            // 
+            // b_Record
+            // 
+            this.b_Record.Location = new System.Drawing.Point(84, 3);
+            this.b_Record.Name = "b_Record";
+            this.b_Record.Size = new System.Drawing.Size(75, 57);
+            this.b_Record.TabIndex = 1;
+            this.b_Record.Text = "RECORD";
+            this.b_Record.UseVisualStyleBackColor = true;
+            this.b_Record.Click += new System.EventHandler(this.b_Record_Click);
+            // 
+            // b_Start
+            // 
+            this.b_Start.Location = new System.Drawing.Point(3, 3);
+            this.b_Start.Name = "b_Start";
+            this.b_Start.Size = new System.Drawing.Size(75, 57);
+            this.b_Start.TabIndex = 0;
+            this.b_Start.Text = "START";
+            this.b_Start.UseVisualStyleBackColor = true;
+            this.b_Start.Click += new System.EventHandler(this.b_Start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pBoxCam);
+            this.Controls.Add(this.mStrip);
+            this.MainMenuStrip = this.mStrip;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mStrip.ResumeLayout(false);
+            this.mStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCam)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mStrip;
+        private System.Windows.Forms.ToolStripMenuItem b_SaveTo;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem videoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox cBoxVideo;
+        private System.Windows.Forms.ToolStripMenuItem audioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox cBoxAudio;
+        private System.Windows.Forms.ToolStripMenuItem b_Exit;
+        private System.Windows.Forms.PictureBox pBoxCam;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button b_Record;
+        private System.Windows.Forms.Button b_Start;
+    }
+}
+
---
+++ Recorder/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Accord.DirectSound;
+using Accord.Video.DirectShow;
+using Accord.Video.FFMPEG;
+using Accord.Audio;
+using System.Threading;
+
+namespace Recorder
+{
+    public partial class Form1 : Form
+    {
+        string path="";       
+        FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+        AudioDeviceCollection audioInDevices = new AudioDeviceCollection(AudioDeviceCategory.Capture);       
+        public VideoCaptureDevice videoSource;
+        AudioCaptureDevice audioInSource;           
+        List<AudioDeviceInfo> audioInDevicesInfo = new List<AudioDeviceInfo>();
+        VideoFileWriter writer = new VideoFileWriter();
+        private DateTime? _firstFrame;
+        private TimeSpan _firstSignal;   
+        
+        bool newSig, newBit, isRecord, bStartRecord = false, bStartShow = false;
+        Bitmap bitmap;
+        Signal signal;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void b_SaveTo_Click(object sender, EventArgs e)
+        {
+            using (var folder = new FolderBrowserDialog())
+            {
+                folder.ShowDialog();
+                path = folder.SelectedPath+"/";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            list_Device();
+        }
+
+        //Получаем список устройст и записываем их в ComboBox
+        private void list_Device()
+        {
+            foreach (var device in videoDevices)
+            {
+                cBoxVideo.Items.Add(device.Name);
+            }
+            foreach (var device in audioInDevices)
+            {
+                cBoxAudio.Items.Add(device.ToString());
+                audioInDevicesInfo.Add(device);
+            }
+            cBoxVideo.SelectedIndex = Convert.ToInt32(0);
+            cBoxAudio.SelectedIndex = Convert.ToInt32(0);
+        }
+
+
+        //Начинаем прослушивать устройста аудио и видео
+        private void b_Start_Click(object sender, EventArgs e)
+        {
+            if (!bStartShow)
+            {
+                bStartShow = true;
+                b_Start.Text = "STOP";
+                videoSource = new VideoCaptureDevice(videoDevices[cBoxVideo.SelectedIndex].MonikerString);
+                videoSource.NewFrame += new Accord.Video.NewFrameEventHandler(VideoSource_NewFrame);
+                videoSource.Start();
+                audioInSource = new AudioCaptureDevice(audioInDevicesInfo[cBoxAudio.SelectedIndex]);
+                audioInSource.NewFrame += AudioInSource_NewFrame;              
+                audioInSource.Start();
+               
+            }
+            else
+            {
+                bStartShow = false;
+                b_Start.Text = "START";
+                videoSource.SignalToStop();
+                audioInSource.SignalToStop();             
+
+            }
+        }
+
+        //Собитие оброботки нового кадра с аудиоустройства
+        private void AudioInSource_NewFrame(object sender, NewFrameEventArgs e)
+        {
+            signal = e.Signal;
+            newSig = true;
+        }
+        //Собитие оброботки нового кадра с видеоустройства
+        private void VideoSource_NewFrame(object sender, Accord.Video.NewFrameEventArgs eventArgs)
+        {
+            pBoxCam.Image = (Bitmap)eventArgs.Frame.Clone();
+            bitmap = (Bitmap)eventArgs.Frame.Clone();
+            newBit = true;
+        }
+
+       
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoSource != null)
+            {
+                if (isRecord == true)
+                {
+                    isRecord = false;
+                }
+                if (videoSource.IsRunning)
+                {
+                    videoSource.SignalToStop();
+                    audioInSource.SignalToStop();
+                }
+            }
+        }
+
+        private void b_Record_Click(object sender, EventArgs e)
+        {
+            if (!bStartRecord)
+            {
+                b_Record.Text = "STOP RECORDING";
+                Thread recording = new Thread(new ThreadStart(rec_Start));
+                isRecord = true;
+                writer = new VideoFileWriter
+                {
+                    Width = bitmap.Width,
+                    Height = bitmap.Height,
+                    AudioCodec = AudioCodec.Aac,
+                    VideoCodec = VideoCodec.Mpeg4,
+                    FrameRate =30,
+                    AudioBitRate = 44100,
+                    AudioLayout = AudioLayout.Mono,
+                    BitRate = 600000,
+                    SampleRate = signal.SampleRate,
+                    FrameSize = 4096
+                };                
+                writer.Open(path+"video.avi");
+                isRecord = true;
+                recording.Start();
+                bStartRecord = true;
+            }
+            else
+            {
+                b_Record.Text = "RECORD";
+                isRecord = false;
+                bStartRecord = false;
+            }
+           
+
+        }
+        private void rec_Start()
+        {
+            //запис первих кадров
+            writer.WriteVideoFrame(bitmap);
+            writer.WriteAudioFrame(signal);   
+            //Определение длины кадра
+            _firstFrame = DateTime.Now;
+            _firstSignal = signal.Duration;
+            newSig = false; newBit = false;
+           
+            //Запис 
+            while (isRecord)
+            {
+                if (newSig)
+                {
+                    _firstSignal += signal.Duration;
+                    writer.WriteAudioFrame(signal, _firstSignal);
+                    newSig = false;
+                    
+                }
+                if (newBit)
+                {
+                    try
+                    {
+                        writer.WriteVideoFrame(bitmap, DateTime.Now - _firstFrame.Value);
+                        newBit = false;
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Console.WriteLine(ex.Message);
+                    }
+
+                }
+
+            }
+            //завершение записи
+            writer.Close();
+            writer.Dispose();          
+            MessageBox.Show("Stop Recording");
+
+        }
+        private void b_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
